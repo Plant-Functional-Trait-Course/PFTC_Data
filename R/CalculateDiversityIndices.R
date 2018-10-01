3da9054cf1dc567b3ade216b0b2653904550e40c
audhalbritter <audhalbritter.com>
2018-10-01 23:40:51 +0200

    initial commit

---
+++ R/CalculateDiversityIndices.R
@@ -0,0 +1,12 @@
+CalculateDiversityIndices <- function(dat){
+  dat2 <- dat %>% 
+    group_by(Year, Site, PlotID) %>%  
+    summarise(n = n(),
+              Richness = n 
+              #Diversity = diversity(Cover), 
+              #N1 = exp(Diversity),
+              #Evenness = Diversity/log(Richness),
+              #sumAbundance = sum(Cover)
+    )
+  return(dat2)
+}
