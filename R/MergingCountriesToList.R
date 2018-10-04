815e6ff3ce2685744c89e9b5935fb0ff25925abf
audhalbritter <audhalbritter.com>
2018-10-04 14:09:20 +0200

    add NO and CO data

--- R/MergingCountriesToList.R
+++ R/MergingCountriesToList.R
@@ -1,30 +1,43 @@
 #### MAKE A LIST FOR ALL COUNTRIES ####
 
-MakeCountryList <- function(metaCH, metaCommunityCH, communityCH, traitCH, 
-                            metaPE, metaCommunityPE, communityPE, traitPE, 
-                            metaCommunitySV, communitySV, traitSV, 
-                            metaNO, metaCommunityNO, communityNO, traitNO
+MakeCountryList <- function(metaCH, metaCommunityCH, communityCH, traitCH, fluxCH,
+                            metaPE, metaCommunityPE, communityPE, traitPE, fluxPE,
+                            metaSV, metaCommunitySV, communitySV, traitSV, fluxSV,
+                            metaNO, metaCommunityNO, communityNO, traitNO, fluxNO,
+                            metaCO, metaCommunityCO, communityCO, traitCO, fluxCO
                             ){
   
   CountryList <- list(China = list(meta = metaCH,
                                    metaCommunity = metaCommunityCH,
                                    community = communityCH,
-                                   trait = traitCH),
+                                   trait = traitCH,
+                                   flux = fluxCH),
                       
                       Peru = list(meta = metaPE,
                                   metaCommunity = metaCommunityPE,
                                   community = communityPE,
-                                  trait = traitPE),
+                                  trait = traitPE,
+                                  flux = fluxPE),
                       
-                      Svalbard = list(metaCommunity = metaCommunitySV,
+                      Svalbard = list(meta = metaSV,
+                                      metaCommunity = metaCommunitySV,
                                       community = communitySV,
-                                      trait = traitSV),
+                                      trait = traitSV,
+                                      flux = fluxSV),
                       
                       Norway = list(meta = metaNO,
                                     metaCommunity = metaCommunityNO,
                                     community = communityNO,
-                                    trait = traitNO)
+                                    trait = traitNO,
+                                    flux = fluxNO),
+                      
+                      Colorado = list(meta = metaCO,
+                                      metaCommunity = metaCommunityCO,
+                                      community = communityCO,
+                                      trait = traitCO,
+                                      flux = fluxCO)
                       )
+  return(CountryList)
   
 }
 
