f1dd7ea4307a30b69acc7731e79532c1cf3282c5
RagnhildGya <rgy042@student.uib.no>
2018-10-08 17:51:27 +0000

    Fix all sorts of cleaning code

--- R/CleanData.R
+++ R/CleanData.R
@@ -36,23 +36,49 @@ CleanChinaCommunity <- function(dat){
 # Cleaning Svalbard community
 CleanSvalbardCommunity <- function(dat){
   dat2 <- dat %>% 
-  rename(Latitude = Latitude_N, Longitude = Longitude_E, Elevation = Elevation_m)
+  rename(Latitude = Latitude_N, Longitude = Longitude_E, Elevation = Elevation_m)%>%
+  mutate(SiteID = paste(Site, Gradient, sep =""),
+           PlotID = paste(Site, Gradient, PlotID, sep=""))
   
   return(dat2)
 }
 
+# Cleaning Svalbard trait
+CleanSvalbardTrait <- function(dat){
+  dat2 <- dat %>% 
+    rename(Latitude = Latitude_N, Longitude = Longitude_E, Elevation = Elevation_m)%>%
+    mutate(SiteID = paste(Site, Gradient, sep =""),
+           PlotID = paste(Site, Gradient, PlotID, sep=""))%>%
+    select(-Treatment, -Genus, -Species)
+  
+  return(dat2)
+}
+
+# Cleaning Peru community
 
 CleanPeruCommunity <- function(dat){
   dat2 <- dat %>% 
-    mutate(Country = "PE")
+    mutate(Country = "PE", 
+           PlotID = paste(PlotID, Treatment, sep=""),
+           Gradient = 1)%>%
+    select(-Genus, -Species, -Treatment)
   return(dat2)
 }
+
+#Cleaning Peru Trait
+
+CleanPeruTrait <- function(dat){
+  dat2 <- dat %>%
+    mutate(PlotID = paste(PlotID, Treatment, sep=""),
+           Gradient = 1) %>%
+  select(-Genus, -Species, -Treatment)
+  return(dat2)
+}  
   
-  
+#Cleaning Norway Meta Community 
 
 CleanNorwayMetaCommunity <- function(dat){
-  dat2 <- dat %>% 
-    filter(!is.na(MedianHeight_cm)) %>% 
+  dat2 <- dat %>%
     rename(Site = SiteID) %>% 
     mutate(Gradient = case_when(Site %in% c("Fau", "Alr", "Ulv") ~ 1,
                                 Site %in% c("Vik", "Hog", "Lav") ~ 2,
@@ -61,10 +87,11 @@ CleanNorwayMetaCommunity <- function(dat){
   return(dat2)
 }  
 
+#Cleaning Norway Community
     
 CleanNorwayCommunity <- function(dat){
   dat2 <- dat %>% 
-    filter(Measure == "Cover") %>% 
+    filter(Measure == "Cover") %>%
     select(-Treatment, -'Nid herb', 'Nid gram', -'Nid rosett', -'Nid seedling', -liver, -lichen, -litter, -soil, -rock, -'#Seedlings', -TotalGraminoids, -totalForbs, -totalBryophytes, -vegetationHeight, -mossHeight, -comment, -'ver seedl', -canum, -totalVascular, totalBryophytes__1, -acro, -pleuro, -totalLichen) %>% 
     gather(key = Taxon, value = Cover, -Site, -Block, -turfID, -subPlot, -year, -date, -Measure, -recorder) %>% 
     filter(!is.na(Cover)) %>% 
@@ -80,6 +107,7 @@ CleanNorwayCommunity <- function(dat){
   return(dat2)
 }
 
+#Cleaning Norway Trait
 
 CleanNorwayTrait <- function(dat){
   dat2 <- dat %>% 
@@ -88,11 +116,13 @@ CleanNorwayTrait <- function(dat){
                                 Site %in% c("Vik", "Hog", "Lav") ~ 2,
                                 Site %in% c("Arh", "Ram", "Gud") ~ 3,
                                 Site %in% c("Ovs", "Ves", "Skj") ~ 4
-    )) 
+    ))%>%
+    select(-Elevation, -Latitude, -Longitude)
   
   return(dat2)
 }
 
+#Cleaning Norway Flux
 
 CleanNorwayFlux <- function(dat){
   dat2 <- dat %>% 
@@ -107,26 +137,45 @@ CleanNorwayFlux <- function(dat){
   return(dat2)
 }
 
+#Cleaning Colorado Community
 
-CleanColoradoFlux <- function(dat){
-dat2 <- dat %>% 
+CleanColoradoCommunity <- function(dat){
+  dat2 <- dat %>%
     filter(!species_or_ground_cover %in% c("Bare (Bare soil + Litter + Dead)", "Rock", "Total Graminoid", "Total Herb", "Total Shrub", "Bare soil", "Litter", "Dead")) %>% 
     select(-X16, -X17, -X18, -X19, -X20, -X21, -X22, -X23, -X24, -X25, -X26, -plot1_count, -plot2_count, -plot3_count, -plot4_count, -plot5_count, -total_site_percent) %>% 
     gather(key = PlotID, value = Cover, -site, -date_yyyymmdd, -species_or_ground_cover, -growth_habit) %>% 
     mutate(Country = "CO",
            Year = 2016) %>% 
-    rename(Site = site, Taxon = species_or_ground_cover, functionalGroup = growth_habit)
-  
+    rename(Site = site, Taxon = species_or_ground_cover, functionalGroup = growth_habit) %>%
+    mutate(PlotID = recode(PlotID, "plot_3_pct" = "plot3_pct"),
+           Gradient = 1)
+  return(dat2)
+}
+
+#Cleaning Colorado Meta Community
+
+CleanColoradoMetaCommunity <- function(dat){
+  dat2 <- dat %>%
+    mutate(PlotID = recode(PlotID, "plot_3_pct" = "plot3_pct"),
+           Gradient = 1)
+    return(dat2)
+}
+
+#Cleaning Colorado Flux
+
+CleanColoradoFlux <- function(dat){
+dat2 <- dat
   return(dat2)
 }  
 
+#Cleaning Colorado Trait
 
 CleanColoradoTrait <- function(dat){
   dat2 <- dat %>% 
-    select(year, site, elev, taxon_std, family, no_of_leaves, leaf_area, wet_mass, dry_mass, SLA, LDMC, WC, height_flower, height_leaf, height, height_2, thickness_1, thickness_2, thickness_3, thickness, pc_C, pc_N, pc_P, d13C, d15N,  C_N,  N_C,  N_P) %>% 
-    rename(Year = year, Site = site, Elevation = elev, PlotID = block, Taxon = taxon_std, Family = family, Leaf_Area_cm2 = leaf_area, Wet_mass_g = wet_mass, Dry_Mass_g = dry_mass, SLA_cm2_g = SLA, Plant_Height_cm = height_flower, Leaf_Thickness_1_mm = thickness_1, Leaf_Thickness_2_mm = thickness_2, Leaf_Thickness_3_mm = thickness_3, Leaf_Thickness_Ave_mm = thickness, C_percent = pc_C, N_percent = pc_N, dC13_percent = d13C, dN15_percent = d15N, CN_ratio = C_N, NC_ratio = N_C, NP_ratio = N_P, P_AVG = pc_P) %>%
-    rename(Site = site)%>%
+    select(year, site, taxon_std, family, no_of_leaves, leaf_area, wet_mass, dry_mass, SLA, LDMC, WC, height_flower, height_leaf, height, height_2, thickness_1, thickness_2, thickness_3, thickness, pc_C, pc_N, pc_P, d13C, d15N,  C_N,  N_C,  N_P) %>% 
+    rename(Year = year, Site = site, Taxon = taxon_std, Family = family, Leaf_Area_cm2 = leaf_area, Wet_mass_g = wet_mass, Dry_Mass_g = dry_mass, SLA_cm2_g = SLA, Plant_Height_cm = height_flower, Leaf_Thickness_1_mm = thickness_1, Leaf_Thickness_2_mm = thickness_2, Leaf_Thickness_3_mm = thickness_3, Leaf_Thickness_Ave_mm = thickness, C_percent = pc_C, N_percent = pc_N, dC13_percent = d13C, dN15_percent = d15N, CN_ratio = C_N, NC_ratio = N_C, NP_ratio = N_P, P_AVG = pc_P) %>%
     filter(Site %in% c("Almont", "CBT", "Road", "Pfeiler", "PBM", "Monument")) %>%
-    mutate(Country = "CO")
+    mutate(Country = "CO")%>%
+    mutate(Gradient = 1)
   return(dat2)
 }  
