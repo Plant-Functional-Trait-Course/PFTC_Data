4541de3dff09bfcb1b89411a13181980e445e532
audhalbritter <audhalbritter.com>
2018-10-12 11:18:20 +0000

    Summarize bootstrapping moments

--- R/CommunityWeightedTraitMeans.R
+++ R/CommunityWeightedTraitMeans.R
@@ -111,21 +111,36 @@ CWM_Bootstrapping <- function(dat, nrep = 100, samplesize = 200){
     group_by(Country, Year, Site, Gradient, BlockID, PlotID, Trait)
   
   
-  BootstrapMoments <- rerun(.n = nrep, sample_n(TraitWeights_all, size = samplesize,  replace = TRUE, weight = TraitWeights_all$weight)) %>%
+  BootstrapMoments_All <- rerun(.n = nrep, sample_n(TraitWeights_all, size = samplesize,  replace = TRUE, weight = TraitWeights_all$weight)) %>%
     bind_rows(.id = "n") %>% 
     group_by(n, add = TRUE) %>% 
     # get all the happy moments
-    summarize(Mean = mean(Value), Variance = var(Value), Skewness = skewness(Value), Kurtosis = kurtosis(Value))
+    summarise(Mean = mean(Value), Variance = var(Value), Skewness = skewness(Value), Kurtosis = kurtosis(Value))
+
+  
+   
+  return(BootstrapMoments_All)
+}
+
+SummarizeBootMoments <- function(BootstrapMoments_All){
+  # calculate means and 
+  BootstrapMoments <- BootstrapMoments_All %>% 
+    group_by(Country, Year, Site, Gradient, BlockID, PlotID, Trait) %>% 
+    summarise(n = n(),
+              meanMean = mean(Mean), CIlow.Mean = meanMean - sd(Mean), CIhigh.Mean = meanMean + sd(Mean),
+              meanVar = mean(Variance), CIlow.Var = meanVar - sd(Variance), CIhigh.Var = meanVar + sd(Variance),
+              meanSkew = mean(Skewness), CIlow.Skew = meanSkew - sd(Skewness), CIhigh.Skew = meanSkew + sd(Skewness),
+              meanKurt = mean(Kurtosis), CIlow.Kurt = meanKurt - sd(Kurtosis), CIhigh.Kurt = meanKurt + sd(Kurtosis)) 
   
   return(BootstrapMoments)
 }
 
 
-
-TraitWeights_all %>% 
-  group_by(Country, Year, Site, Gradient, BlockID, PlotID, Trait, Taxon) %>% 
-  slice(1) %>% 
-  group_by(Country, Year, Site, Gradient, BlockID, PlotID, Trait) %>% summarise(covered = sum(Cover)) %>% mutate(percent_cover = covered/sumCover * 100) %>% arrange(percent_cover) %>% pn
+#NEEDS TO BE INCORPORATED IN BOOTSTRAPPING FUNCTION !!!!!
+#TraitWeights_all %>% 
+  #group_by(Country, Year, Site, Gradient, BlockID, PlotID, Trait, Taxon) %>% 
+  #slice(1) %>% 
+  #group_by(Country, Year, Site, Gradient, BlockID, PlotID, Trait) %>% summarise(covered = sum(Cover)) %>% mutate(percent_cover = covered/sumCover * 100) %>% arrange(percent_cover) %>% pn
 
 
 #### Filtering out turfs with less than 70% of the community present ###
