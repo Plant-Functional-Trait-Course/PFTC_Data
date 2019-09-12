65cde44a20882c7bc927eb6e967316ceb9ffde9e
audhalbritter <audhalbritter.com>
2019-09-12 07:29:50 +0200

    rename folder

--- EunquistLab_CNP_Workflow/RunWorkflowScript.R
+++
@@ -1,9 +0,0 @@
-####################################
-### Run Enquist lab CNP workflow ###
-####################################
-
-# source all the functions
-source("EunquistLab_CNP_Workflow/EnquistLab_CNP_Workflow.R")
-
-# Run script
-make_report(import_path_name = "isotope_data", file_name = "CNP_Results")
