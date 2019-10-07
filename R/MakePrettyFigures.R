ac37d8749aeaea58540ed7f15edc9299522c35a0
audhalbritter <audhalbritter.com>
2019-10-07 07:57:55 +0200

    fix bootstrap moment figure

--- R/MakePrettyFigures.R
+++ R/MakePrettyFigures.R
@@ -1,26 +1,26 @@
-MakeFigure <- function(TraitMeans){
-  myplot <- TraitMeans %>% 
+MakeFigure <- function(BootstrapMoments){
+  myplot <- BootstrapMoments %>% 
     filter(!is.na(Trait), !Trait %in% c("P_Co_Var", "P_Std_Dev", "NC_ratio", "NP_ratio")) %>% 
     ungroup() %>% 
     mutate(Trait = factor(Trait, level = c("Wet_Mass_g", "Dry_Mass_g", "Leaf_Area_cm2", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent"))) %>% 
     mutate(CG = paste(Country, Gradient, sep = "")) %>% 
-    ggplot(aes(x = bio1, y = CWTraitMean, colour = CG)) +
+    ggplot(aes(x = Site, y = meanMean, colour = CG)) +
     geom_point() +
     geom_smooth(method = "lm", se = FALSE) +
     labs(x = "Mean annual Temperature °C", y = "Community weighted trait mean") +
-    facet_wrap(~ Trait, scales = "free_y")
+    facet_grid(Country ~ Trait, scales = "free_y")
   return(myplot)
 }
 
-MakeMeanFigure <- function(CW_Means_Bootstrapped_Bio){
-  myplot <- CW_Means_Bootstrapped_Bio %>% 
-  filter(Trait %in% c("Wet_Mass_g", "Dry_Mass_g", "Leaf_Area_cm2", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent")) %>% 
-  mutate(Trait = factor(Trait, level = c("Wet_Mass_g", "Dry_Mass_g", "Leaf_Area_cm2", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent"))) %>% 
+MakeMeanFigure <- function(BootstrapMoments_Bio){
+  myplot <- BootstrapMoments_Bio %>% 
+    filter(Trait %in% c("Wet_Mass_g_log", "Dry_Mass_g_log", "Leaf_Area_cm2_log", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm_log", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent")) %>% 
+    mutate(Trait = factor(Trait, level = c("Wet_Mass_g_log", "Dry_Mass_g_log", "Leaf_Area_cm2_log", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm_log", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent"))) %>% 
   mutate(CG = paste(Country, Gradient, sep = "")) %>% 
-  ggplot(aes(x = AnnMeanTemp, y = mean, colour = CG)) +
-  geom_point() +
+  ggplot(aes(x = MeanTempWarmQuart, y = meanMean, colour = CG)) +
+  geom_point(alpha = 0.3) +
   geom_smooth(method = "lm", se = FALSE) +
-  labs(x = "Mean annual Temperature °C", y = "Community weighted trait mean") +
+  labs(x = "Mean temperature of warmest quarter (°C)", y = "Community weighted trait mean") +
   facet_wrap(~ Trait, scales = "free_y")
   return(myplot)
 }
