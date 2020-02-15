85f43f2af73df19c3169b3722c4d472b1b7339b0
audhalbritter <audhalbritter.com>
2020-02-15 10:45:44 +0100

    move to functions

---
+++ R/Functions/CWTM_Bootstrapping.R
@@ -0,0 +1,19 @@
+### BOOTSTRAPPING METHOD FOR CWM
+
+# Impute Traits
+BootstrappedCWM <- function(CountryList){
+  
+  HappyMoments <- map(.x = CountryList, ~trait_impute(comm = .x$community, 
+                                                      traits = .x$trait %>% select(-Gradient, -Year), 
+                                                      scale_hierarchy = .x$trait_hierarchy,
+                                                      taxon_col = "Taxon",
+                                                      trait_col = "Trait",
+                                                      value_col = "Value_trans", 
+                                                      abundance_col = "Cover", 
+                                                      other_col = c("Gradient", "Year"))) %>% 
+    map_df(.x = ., ~trait_np_bootstrap(imputed_traits = .))
+  
+  return(HappyMoments)
+  
+}
+
