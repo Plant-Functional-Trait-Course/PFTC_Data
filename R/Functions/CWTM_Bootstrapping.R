6066c7724ce52d3e90f75afe6224e0ad8a05d475
audhalbritter <audhalbritter.com>
2020-02-15 22:20:50 +0100

    use transformed traits

--- R/Functions/CWTM_Bootstrapping.R
+++ R/Functions/CWTM_Bootstrapping.R
@@ -3,14 +3,15 @@
 # Impute Traits
 BootstrappedCWM <- function(CountryList){
   
-  HappyMoments <- map(.x = CountryList, ~trait_impute(comm = .x$community, 
-                                                      traits = .x$trait %>% select(-Gradient, -Year), 
-                                                      scale_hierarchy = .x$trait_hierarchy,
-                                                      taxon_col = "Taxon",
-                                                      trait_col = "Trait",
-                                                      value_col = "Value_trans", 
-                                                      abundance_col = "Cover", 
-                                                      other_col = c("Gradient", "Year"))) %>% 
+  HappyMoments <- map(.x = CountryList, 
+                      ~trait_impute(comm = .x$community,
+                                    traits = .x$trait %>% select(-Gradient, -Year), 
+                                    scale_hierarchy = .x$trait_hierarchy,
+                                    taxon_col = "Taxon",
+                                    trait_col = "Trait_trans",
+                                    value_col = "Value_trans", 
+                                    abundance_col = "Cover", 
+                                    other_col = c("Gradient", "Year"))) %>% 
     map_df(.x = ., ~trait_np_bootstrap(imputed_traits = .))
   
   return(HappyMoments)
