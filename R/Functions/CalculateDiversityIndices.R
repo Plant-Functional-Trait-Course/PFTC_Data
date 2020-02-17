69ad8fcebba3446f80c4962e491085af74c064b4
audhalbritter <audhalbritter.com>
2020-02-17 22:30:13 +0100

    spPool regional add model

--- R/Functions/CalculateDiversityIndices.R
+++ R/Functions/CalculateDiversityIndices.R
@@ -1,3 +1,4 @@
+# Calculate Diversity Indices
 CalculateDiversityIndices <- function(CountryList){
   # add plot id for colorado
   cols <- c(PlotID = "1")
@@ -5,13 +6,10 @@ CalculateDiversityIndices <- function(CountryList){
   diversity <- CountryList$community %>% 
     # Calculate species pool per gradient
     group_by(Country) %>% 
-    mutate(spPool_global = n()) %>% 
-    group_by(Country, Site) %>% 
-    mutate(spPool_site = n()) %>% 
+    mutate(spPool_regional = n()) %>% 
     add_column(!!!cols[!names(cols) %in% names(.)]) %>% 
-    group_by(Country, Gradient, Site, BlockID, PlotID, spPool_global, spPool_site) %>%  
-    summarise(n = n(),
-              Richness = n,
+    group_by(Country, Gradient, Site, BlockID, PlotID, spPool_regional) %>%  
+    summarise(Richness = n(),
               Diversity = diversity(Cover), 
               N1 = exp(Diversity),
               Evenness = Diversity/log(Richness),
@@ -20,4 +18,18 @@ CalculateDiversityIndices <- function(CountryList){
   return(diversity)
 }
 
-        
+
+# Test relashionship of diversity and Elevation, MeanTemp and AnnPrec
+# RegressionDiversityIndeces <- function(Diversity){
+#   Diversity %>%
+#     mutate(CG = paste(Country, Gradient, sep = "_")) %>%
+#     mutate(PropAbundance = Richness / spPool_regional) %>%
+#     pivot_longer(cols = c("PropAbundance", "Richness", "Diversity", "N1", "Evenness", "sumAbundance"), names_to = "Index", values_to = "Value") %>%
+#     group_by(CG, Index) %>%
+#     nest() %>% 
+#     mutate(mod = map(data, lm(Value ~ Elevation, data = .x)))
+#     map(., tidy, fit)
+# 
+#   return(Result)
+# }
+    
