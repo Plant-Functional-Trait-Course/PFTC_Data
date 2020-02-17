c52625814eb15f7639396215f97f69696d09b44a
audhalbritter <audhalbritter.com>
2020-02-17 22:31:02 +0100

    remove PlotID fix, fixed it in cleaning step

--- R/Functions/MultivatiateAnalysis.R
+++ R/Functions/MultivatiateAnalysis.R
@@ -4,12 +4,8 @@
 
 # NMDS ordination
 NMDSOrdination <- function(CountryList){
-  set.seed(42)
-  cols <- c(PlotID = "1")
   
   comm_wide <- CountryList$community %>% 
-    # add plotID in colorado
-    add_column(!!!cols[!names(cols) %in% names(.)]) %>% 
     pivot_wider(names_from = Taxon, values_from = Cover, values_fill = list(Cover = 0)) 
   comm_wide_spp <- comm_wide %>% select(-(Country:PlotID))
 
@@ -17,11 +13,11 @@ NMDSOrdination <- function(CountryList){
 
   fNMDS <- fortify(NMDS) %>% 
     filter(Score == "sites") %>% 
-    as.tibble() %>% 
-    bind_cols(comm_wide) %>% 
-    select(Country:PlotID, NMDS1, NMDS2)
+    bind_cols(comm_wide %>% select(Country:PlotID))
   
   return(fNMDS)
 }
+
+
 
 
