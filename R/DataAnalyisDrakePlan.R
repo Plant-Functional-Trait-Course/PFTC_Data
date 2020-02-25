83dafceb1c6741030ba6aeb0f70c32253ca80b42
audhalbritter <audhalbritter.com>
2020-02-25 23:23:07 +0100

    add diversity and ordination

--- R/DataAnalyisDrakePlan.R
+++ R/DataAnalyisDrakePlan.R
@@ -3,5 +3,10 @@
 
 DataAnalysisDrakePlan <- drake_plan(
   
+  # Test realationship of Diversity along gradients
+  ResultsDiversity = RegressionDiversityIndeces(Diversity),
+  
+  # NMDS Ordination
+  fNMDS = map_df(CountryList, NMDSOrdination)
 
 ) 
