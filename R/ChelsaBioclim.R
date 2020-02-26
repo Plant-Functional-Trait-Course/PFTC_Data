3d9765c0c6a7500521023c53a90797779233ce4a
audhalbritter <audhalbritter.com>
2020-02-26 11:37:33 +0100

    add temp for svalbard sites

--- R/ChelsaBioclim.R
+++ R/ChelsaBioclim.R
@@ -48,3 +48,47 @@ MetaClimate <- MetaData %>%
   bind_cols(climate)
 
 write_csv(x = MetaClimate, path = "data/MetaClimate.csv", col_names = TRUE)
+
+
+
+# Svalbard
+metaSV <- Climate %>% 
+  filter(Country == "SV") %>% 
+  select(Gradient, Site, Elevation) %>% 
+  arrange(Gradient, Elevation)
+
+coords <- data.frame(Longitude = c(15.68663, 15.67255, 15.91093, 15.39917, 15.36402, 15.37586),
+                     Latitude = c(78.21716, 78.19637, 78.16775, 78.24738, 78.23936, 78.20929))
+
+# Elev
+elev <- raster::getData('worldclim', var = "alt", res = 0.5, lon = 15, lat = 78)
+
+# Mean Ann Temp
+rastlist <- list.files(path = "/Volumes/fis003/Projects/PFTC/ClimateData/", pattern = ".tif", 
+                       all.files = TRUE, full.names = TRUE)
+
+allrasters <- map(rastlist[1], raster::raster)
+
+TempElev <- map(.x = allrasters, .f = raster::extract, coords) %>% 
+  do.call("rbind", .) %>% 
+  t() %>% as_tibble() %>% 
+  rename("MeanTemp" = "V1") %>% 
+  mutate(MeanTemp = MeanTemp /10) %>% 
+  bind_cols(Elev = raster::extract(elev, coords),
+            Gradient = c(rep("C", 3), rep("B", 3)))
+
+# Regression and predict new Temp
+mod <- lm(MeanTemp ~ Elev, data = TempElev)
+new <- data.frame(Elev = metaSV$Elevation)
+metaSV$MeanTempSV <- predict(mod, new)
+ 
+dd <- Climate %>% 
+  left_join(metaSV, by = c("Gradient", "Site", "Elevation")) %>% 
+  mutate(MeanTemp = if_else(Country == "SV", MeanTempSV, MeanTemp)) %>% 
+  select(-MeanTempSV)
+
+write_csv(dd, path = "data/MetaClimate.csv", col_names = TRUE)
+
+
+
+
