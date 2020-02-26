5aeb8442a16ed02f036cc5d6d91f2631fae9ee63
audhalbritter <audhalbritter.com>
2020-02-26 16:14:36 +0100

    add coverage plot at site level

--- R/Functions/MakePrettyFigures.R
+++ R/Functions/MakePrettyFigures.R
@@ -5,6 +5,7 @@ MakeCoverageFigure <- function(ImputetTraits){
   CoverageFigure <- map(.x = ImputetTraits, ~ filter(., Trait == "Leaf_Area_cm2")) %>% 
     map_df(fortify, .id = "FullCountry") %>% 
   mutate(level = factor(level, levels = c("Country", "Site", "BlockID", "PlotID"))) %>% 
+  #mutate(level = factor(level, levels = c("Country", "Site"))) %>% 
     ggplot(aes(x = .id, y = s, fill = level)) +
     geom_col() +
     scale_y_continuous(expand = c(0, 0)) +
@@ -18,82 +19,88 @@ MakeCoverageFigure <- function(ImputetTraits){
 }
 
 
+MakeCoverageSiteLevel <- function(ImputetTraits){
+  
+  CoverageSiteLevel <- map(.x = ImputetTraits, ~ filter(., Trait == "Leaf_Area_cm2")) %>% 
+    map_df(fortify, .id = "FullCountry") %>% 
+    # recode block and plot ID to site level
+    mutate(level = recode(level, "BlockID" = "Site", "PlotID" = "Site")) %>% 
+    mutate(level = factor(level, levels = c("Country", "Site"))) %>% 
+    ggplot(aes(x = .id, y = s, fill = level)) +
+    geom_col() +
+    scale_y_continuous(expand = c(0, 0)) +
+    scale_fill_viridis_d() +
+    labs(x = "", y = "Proportion of cover", fill = "Data source") +
+    facet_wrap(~FullCountry, scales = "free_x") +
+    theme_bw() +
+    theme(axis.text.x = element_blank())
+  
+  return(CoverageSiteLevel)
+}
 
 
-
-
-
-
-
-
-
-
-MakeFigure <- function(BootstrapMoments){
-  myplot <- BootstrapMoments %>% 
-    filter(!is.na(Trait), !Trait %in% c("P_Co_Var", "P_Std_Dev", "NC_ratio", "NP_ratio")) %>% 
-    ungroup() %>% 
-    mutate(Trait = factor(Trait, level = c("Wet_Mass_g", "Dry_Mass_g", "Leaf_Area_cm2", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent"))) %>% 
-    mutate(CG = paste(Country, Gradient, sep = "")) %>% 
-    ggplot(aes(x = Site, y = meanMean, colour = CG)) +
+# Plot summarised moments - mean
+MakeMeanFigure <- function(SummarisedMoments){
+  
+  # filter moment
+  
+  GradientMeanPlot <- SummarisedMoments %>% 
+    filter(!Trait_trans %in% c("Wet_Mass_g_log", "CN_ratio", "dC13_percent", "dN15_percent", "P_percent", "C_percent", "N_percent")) %>% 
+    mutate(Trait_trans = factor(Trait_trans, level = c("Plant_Height_cm_log", "Dry_Mass_g_log", "Leaf_Area_cm2_log", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g"))) %>% 
+    mutate(CG = paste(Country, Gradient, sep = ""),
+           FullCountry = recode(Country, "CH" = "China", "CO" = "Colorado", "NO" = "Norway", "PE" = "Peru", "SV" = "Svalbard")
+           # CGnum = case_when(CG %in% c("SVC", "SVB") ~ 1,
+           #                   CG %in% c("NO1", "NO2", "NO3", "NO4") ~ 2,
+           #                   CG %in% c("CO1") ~ 3,
+           #                   CG %in% c("CH1") ~ 4,
+           #                   CG %in% c("PEC", "PEB") ~ 5)
+           ) %>% 
+    mutate(GradNum = case_when(Gradient %in% c("1", "C") ~ 1,
+                               Gradient %in% c("2", "B") ~ 2,
+                               Gradient %in% c("3") ~ 3,
+                               Gradient %in% c("4") ~ 4)) %>% 
+    filter(!CG %in% c("PEB", "SVB")) %>% 
+    ggplot(aes(x = MeanTemp, y = Mean, color = FullCountry, linetype = factor(GradNum))) +
     geom_point() +
+    scale_colour_viridis_d(option = "plasma", end = 0.9) +
+    labs(x = "Mean annual temperature in °C", y = "", title = "Mean trait value", colour = "") +
     geom_smooth(method = "lm", se = FALSE) +
-    labs(x = "Mean annual Temperature °C", y = "Community weighted trait mean") +
-    facet_grid(Country ~ Trait, scales = "free_y")
-  return(myplot)
-}
-
-MakeMeanFigure <- function(BootstrapMoments_Bio){
-  myplot <- BootstrapMoments_Bio %>% 
-    filter(Trait %in% c("Wet_Mass_g_log", "Dry_Mass_g_log", "Leaf_Area_cm2_log", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm_log", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent")) %>% 
-    mutate(Trait = factor(Trait, level = c("Wet_Mass_g_log", "Dry_Mass_g_log", "Leaf_Area_cm2_log", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm_log", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent"))) %>% 
-  mutate(CG = paste(Country, Gradient, sep = "")) %>% 
-  ggplot(aes(x = MeanTempWarmQuart, y = meanMean, colour = CG)) +
-  geom_point(alpha = 0.3) +
-  geom_smooth(method = "lm", se = FALSE) +
-  labs(x = "Mean temperature of warmest quarter (°C)", y = "Community weighted trait mean") +
-  facet_wrap(~ Trait, scales = "free_y")
-  return(myplot)
-}
-
-MakeVarFigure <- function(CW_Means_Bootstrapped_Bio){
-  myplot <- CW_Means_Bootstrapped_Bio %>% 
-    filter(Trait %in% c("Wet_Mass_g", "Dry_Mass_g", "Leaf_Area_cm2", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent")) %>% 
-    mutate(Trait = factor(Trait, level = c("Wet_Mass_g", "Dry_Mass_g", "Leaf_Area_cm2", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent"))) %>% 
-    mutate(CG = paste(Country, Gradient, sep = "")) %>% 
-    ggplot(aes(x = AnnMeanTemp, y = var, colour = CG)) +
-    geom_point() +
-    geom_smooth(method = "lm", se = FALSE) +
-    labs(x = "Mean annual Temperature °C", y = "Community weighted trait variation") +
-    facet_wrap(~ Trait, scales = "free_y")
-  return(myplot)
-}
-
-MakeSkewFigure <- function(CW_Means_Bootstrapped_Bio){
-  myplot <- CW_Means_Bootstrapped_Bio %>% 
-    filter(Trait %in% c("Wet_Mass_g", "Dry_Mass_g", "Leaf_Area_cm2", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent")) %>% 
-    mutate(Trait = factor(Trait, level = c("Wet_Mass_g", "Dry_Mass_g", "Leaf_Area_cm2", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent"))) %>% 
-    mutate(CG = paste(Country, Gradient, sep = "")) %>% 
-    ggplot(aes(x = AnnMeanTemp, y = skew, colour = CG)) +
-    geom_point() +
-    geom_smooth(method = "lm", se = FALSE) +
-    labs(x = "Mean annual Temperature °C", y = "Community weighted trait skewness") +
-    facet_wrap(~ Trait, scales = "free_y")
-  return(myplot)
+    facet_wrap( ~ Trait_trans, scales = "free_y") +
+    guides(linetype = FALSE) +
+    theme_bw() +
+    theme(legend.position = "top")
 }
 
 
-MakeKurtFigure <- function(CW_Means_Bootstrapped_Bio){
-  myplot <- CW_Means_Bootstrapped_Bio %>% 
-    filter(Trait %in% c("Wet_Mass_g", "Dry_Mass_g", "Leaf_Area_cm2", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent")) %>% 
-    mutate(Trait = factor(Trait, level = c("Wet_Mass_g", "Dry_Mass_g", "Leaf_Area_cm2", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g", "Plant_Height_cm", "C_percent", "N_percent", "CN_ratio", "P_AVG", "dN15_percent", "dC13_percent"))) %>% 
-    mutate(CG = paste(Country, Gradient, sep = "")) %>% 
-    ggplot(aes(x = AnnMeanTemp, y = kurt, colour = CG)) +
-    geom_point() +
-    geom_smooth(method = "lm", se = FALSE) +
-    labs(x = "Mean annual Temperature °C", y = "Community weighted trait kurtosis") +
-    facet_wrap(~ Trait, scales = "free_y")
-  return(myplot)
+
+# Plot summarized moments - variance
+MakeVarFigure <- function(){
+  GradientVarPlot <- GradientMeanPlot %+% geom_point(aes(y = Var)) +
+    labs(y = "Variance in trait value")
+  
+  return(GradientVarPlot)
 }
+
+
+# Smiley plot - kurtosis and skewness
+SmileyPlot <- function(){
+  SummarisedMoments %>% 
+    filter(Trait_trans %in% c("Plant_Height_cm_log", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g")) %>% 
+    mutate(Trait_trans = factor(Trait_trans, level = c("Plant_Height_cm_log","Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g"))) %>% 
+    mutate(FullCountry = recode(Country, "CH" = "China", "CO" = "Colorado", "NO" = "Norway", "PE" = "Peru", "SV" = "Svalbard")) %>% 
+    ggplot(aes(x = Skew, y = Kurt, color = FullCountry)) +
+    geom_point(alpha = 0.4, size = 3) +
+    scale_colour_viridis_d(option = "plasma", end = 0.9) +
+    facet_wrap( ~ Trait_trans) +
+    theme_bw() +
+    theme(legend.position = "top")
+}
+
+
+
+
+
+
 
 
 
