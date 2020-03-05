78ea7492a03ba19b267f545cf5aad9b1fd9ddfdd
audhalbritter <audhalbritter.com>
2020-03-05 16:39:44 +0100

    momemt figure

--- R/Functions/MakePrettyFigures.R
+++ R/Functions/MakePrettyFigures.R
@@ -4,15 +4,14 @@ MakeCoverageFigure <- function(ImputetTraits){
   
   CoverageFigure <- map(.x = ImputetTraits, ~ filter(., Trait == "Leaf_Area_cm2")) %>% 
     map_df(fortify, .id = "FullCountry") %>% 
-  mutate(level = factor(level, levels = c("Country", "Site", "BlockID", "PlotID"))) %>% 
-  #mutate(level = factor(level, levels = c("Country", "Site"))) %>% 
+    filter(Country %in% c("NO", "SV")) %>% 
+    mutate(level = factor(level, levels = c("Country", "Site", "BlockID", "PlotID"))) %>% 
     ggplot(aes(x = .id, y = s, fill = level)) +
     geom_col() +
     scale_y_continuous(expand = c(0, 0)) +
     scale_fill_viridis_d() +
     labs(x = "", y = "Proportion of cover", fill = "Data source") +
     facet_wrap(~FullCountry, scales = "free_x") +
-    theme_bw() +
     theme(axis.text.x = element_blank())
   
   return(CoverageFigure)
@@ -21,7 +20,7 @@ MakeCoverageFigure <- function(ImputetTraits){
 
 MakeCoverageSiteLevel <- function(ImputetTraits){
   
-  CoverageSiteLevel <- map(.x = ImputetTraits, ~ filter(., Trait == "Leaf_Area_cm2")) %>% 
+  CoverageSiteLevel <- map(.x = ImputetTraits, ~ filter(., Trait_trans == "Leaf_Area_cm2_log")) %>% 
     map_df(fortify, .id = "FullCountry") %>% 
     # recode block and plot ID to site level
     mutate(level = recode(level, "BlockID" = "Site", "PlotID" = "Site")) %>% 
@@ -32,7 +31,6 @@ MakeCoverageSiteLevel <- function(ImputetTraits){
     scale_fill_viridis_d() +
     labs(x = "", y = "Proportion of cover", fill = "Data source") +
     facet_wrap(~FullCountry, scales = "free_x") +
-    theme_bw() +
     theme(axis.text.x = element_blank())
   
   return(CoverageSiteLevel)
@@ -40,45 +38,69 @@ MakeCoverageSiteLevel <- function(ImputetTraits){
 
 
 # Plot summarised moments - mean
-MakeMeanFigure <- function(SummarisedMoments){
-  
-  # filter moment
-  
-  GradientMeanPlot <- SummarisedMoments %>% 
-    filter(!Trait_trans %in% c("Wet_Mass_g_log", "CN_ratio", "dC13_percent", "dN15_percent", "P_percent", "C_percent", "N_percent")) %>% 
-    mutate(Trait_trans = factor(Trait_trans, level = c("Plant_Height_cm_log", "Dry_Mass_g_log", "Leaf_Area_cm2_log", "Leaf_Thickness_Ave_mm", "LDMC", "SLA_cm2_g"))) %>% 
+MakeMeanFigure <- function(SummarisedMoments_Site){
+
+  GradientMeanPlot <- SummarisedMoments_Site %>% 
+    filter(Trait_trans %in% c("Plant_Height_cm_log")) %>% 
+    select(-c(MeanDiurnalRange:PrecColdQuart)) %>% 
+    pivot_longer(cols = c("Mean", "Var", "Skew", "Kurt"), names_to = "Moment", values_to = "Value") %>% 
+    mutate(Moment = recode(Moment, "Mean" = "mean", "Var" = "variance", "Skew" = "skewness", "Kurt" = "kurtosis"),
+           Moment = factor(Moment, levels = c("mean", "variance", "skewness", "kurtosis"))) %>% 
     mutate(CG = paste(Country, Gradient, sep = ""),
-           FullCountry = recode(Country, "CH" = "China", "CO" = "Colorado", "NO" = "Norway", "PE" = "Peru", "SV" = "Svalbard")
-           # CGnum = case_when(CG %in% c("SVC", "SVB") ~ 1,
-           #                   CG %in% c("NO1", "NO2", "NO3", "NO4") ~ 2,
-           #                   CG %in% c("CO1") ~ 3,
-           #                   CG %in% c("CH1") ~ 4,
-           #                   CG %in% c("PEC", "PEB") ~ 5)
-           ) %>% 
+           FullCountry = recode(Country, "CH" = "China", "CO" = "Colorado", "NO" = "Norway", "PE" = "Peru", "SV" = "Svalbard")) %>% 
     mutate(GradNum = case_when(Gradient %in% c("1", "C") ~ 1,
-                               Gradient %in% c("2", "B") ~ 2,
+                               Gradient %in% c("2") ~ 2,
                                Gradient %in% c("3") ~ 3,
                                Gradient %in% c("4") ~ 4)) %>% 
-    filter(!CG %in% c("PEB", "SVB")) %>% 
-    ggplot(aes(x = MeanTemp, y = Mean, color = FullCountry, linetype = factor(GradNum))) +
+    left_join(RegMoment %>% select(-data, -mod), by = c("Country", "Trait_trans", "Moment")) %>% 
+    filter(Moment == "mean") %>% 
+    ggplot(aes(x = MeanTemp, y = Value, color = FullCountry, linetype = OverlapZero)) +
     geom_point() +
     scale_colour_viridis_d(option = "plasma", end = 0.9) +
-    labs(x = "Mean annual temperature in °C", y = "", title = "Mean trait value", colour = "") +
+    labs(x = "Mean annual temperature in °C", y = "", title = "log(Plant height in cm)", colour = "") +
     geom_smooth(method = "lm", se = FALSE) +
-    facet_wrap( ~ Trait_trans, scales = "free_y") +
+    #facet_wrap( ~ Moment, scales = "free_y") +
     guides(linetype = FALSE) +
     theme_bw() +
-    theme(legend.position = "top")
+    theme(legend.position = "top",
+          axis.title = element_text(size = 15),
+          axis.text = element_text(size = 12),
+          legend.text = element_text(size = 12))
 }
 
 
 
 # Plot summarized moments - variance
-MakeVarFigure <- function(){
-  GradientVarPlot <- GradientMeanPlot %+% geom_point(aes(y = Var)) +
-    labs(y = "Variance in trait value")
+MakeMomentFigure <- function(){
   
-  return(GradientVarPlot)
+  HigherMomentPlot <- SummarisedMoments_Site %>% 
+    filter(Trait_trans %in% c("SLA_cm2_g")) %>% 
+    select(-c(MeanDiurnalRange:PrecColdQuart)) %>% 
+    pivot_longer(cols = c("Mean", "Var", "Skew", "Kurt"), names_to = "Moment", values_to = "Value") %>% 
+    mutate(Moment = recode(Moment, "Mean" = "mean", "Var" = "variance", "Skew" = "skewness", "Kurt" = "kurtosis"),
+           Moment = factor(Moment, levels = c("mean", "variance", "skewness", "kurtosis"))) %>% 
+    mutate(CG = paste(Country, Gradient, sep = ""),
+           FullCountry = recode(Country, "CH" = "China", "CO" = "Colorado", "NO" = "Norway", "PE" = "Peru", "SV" = "Svalbard")) %>% 
+    mutate(GradNum = case_when(Gradient %in% c("1", "C") ~ 1,
+                               Gradient %in% c("2") ~ 2,
+                               Gradient %in% c("3") ~ 3,
+                               Gradient %in% c("4") ~ 4)) %>% 
+    left_join(RegMoment, by = c("Country", "Trait_trans", "Moment")) %>% 
+    filter(Moment != "mean") %>% 
+    ggplot(aes(x = MeanTemp, y = Value, color = FullCountry, linetype = OverlapZero)) +
+    geom_point() +
+    scale_colour_viridis_d(option = "plasma", end = 0.9) +
+    labs(x = "Mean annual temperature in °C", y = "", title = "log(Plant height in cm)", colour = "") +
+    geom_smooth(method = "lm", se = FALSE) +
+    facet_wrap( ~ Moment, scales = "free_y") +
+    guides(linetype = FALSE) +
+    theme_bw() +
+    theme(legend.position = "top",
+          axis.title = element_text(size = 15),
+          axis.text = element_text(size = 12),
+          legend.text = element_text(size = 12))
+  
+  return(HigherMomentPlot)
 }
 
 
@@ -118,9 +140,6 @@ DiverstiyPlot <- function(Diversity){
   
   return(DivPlot)
 }
-
-
-
 
 
 
